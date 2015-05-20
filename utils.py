2c2b577f8402dce62c4b2e3b117c56d72127e6d8
Jonas Wielicki <j.wielicki@sotecware.net>
2015-05-20 14:54:05 +0200

    Fix connecting to the mailserver
    We need a way to verify the Certificate (done using MAIL_CAFILE config
    variable) and we need to authenticate against the server (using
    MAIL_USER and MAIL_PASSWORD)

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import logging
+import ssl
+
+logger = logging.getLogger(__name__)
+
 from email.utils import formatdate
 from email.mime.text import MIMEText
 import smtplib
 from config import (
-    MAIL_SERVER, MAIL_PORT,
+    MAIL_SERVER, MAIL_PORT, MAIL_USER, MAIL_PASSWORD, MAIL_CAFILE,
     INVITATION_SENDER, INVITATION_SUBJECT, INVITATION_REDEEM_BASEURL
 )
 
@@ -27,13 +32,22 @@ Follow this link to redeem your invitation:
     mail['Subject'] = INVITATION_SUBJECT
     mail['Date'] = formatdate(localtime=True)
 
+    ctx = ssl.create_default_context(cafile=MAIL_CAFILE)
+    ctx.verify_mode = ssl.CERT_REQUIRED
+
     try:
-        smtp = smtplib.SMTP()
-        smtp.connect(host=MAIL_SERVER, port=MAIL_PORT)
+        # we have to pass the host + port to the constructor, for SSL to work
+        # this might in fact be a bug in the python stdlib
+        smtp = smtplib.SMTP(host=MAIL_SERVER, port=MAIL_PORT)
+        smtp.starttls(context=ctx)
+        if MAIL_USER and MAIL_PASSWORD:
+            print("logged in")
+            smtp.login(MAIL_USER, MAIL_PASSWORD)
         smtp.sendmail(INVITATION_SENDER, target, mail.as_string(0))
         smtp.close()
         return True
     except IOError:
+        logger.exception("failed to send mail")
         return False
 
 

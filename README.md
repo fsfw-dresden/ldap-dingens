efb68c16b1ae894f7578549b2dc759bec829f747
Dominik Pataky <git@netdecorator.org>
2015-05-20 08:16:02 +0200

    Add LICENCE, add README

---
+++ README.md
@@ -0,0 +1,6 @@
+#FSFW LDAP-Dingens
+Tool used to create, view and modify LDAP users.
+
+As a registered user, you can create invitations which are sent to a not yet 
+registered user. The receiver can then redeem the invitation token to provide
+more user profile data and create the account.
--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+FSFW LDAP-Dingens
+Copyright (C) 2015 Dominik Pataky <mail@netdecorator.org>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+This licence text applies to all other files in this repository as well.
+
+The source code which is used to run the application at fsfw-dresden.de is
+available at https://github.com/fsfw-dresden/ldap-dingens
+"""
+
 from flask import Flask, flash, redirect, render_template, url_for
 from flask.ext.login import (
     LoginManager, login_required, login_user, logout_user

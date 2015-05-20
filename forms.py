4050158fa16f235951d8093bdf0d65f19825a4e8
Jonas Wielicki <j.wielicki@sotecware.net>
2015-05-20 15:32:09 +0200

    Add password field to redeem form

--- forms.py
+++ forms.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
 from flask.ext.wtf import Form
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, ValidationError
+
+
+class SameAs:
+    def __init__(self, other_field):
+        self.other_field = other_field
+
+    def __call__(self, form, field):
+        value = getattr(form, self.other_field).data
+        if value != field.data:
+            raise ValidationError("Confirmation must match password")
 
 
 class CreateInviteForm(Form):
@@ -15,3 +25,7 @@ class CreateInviteForm(Form):
 class RedeemForm(Form):
     last_name = StringField('Last name', validators=[DataRequired()])
     first_name = StringField('First name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm password',
+                                     validators=[DataRequired(),
+                                                 SameAs("password")])

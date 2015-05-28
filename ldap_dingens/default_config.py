46009d28e2b6285722287ccbeaa8d2f9c6c47fde
Jonas Wielicki <j.wielicki@sotecware.net>
2015-05-28 16:30:55 +0200

    Remove pointless default values from DefaultConfiguration

--- ldap_dingens/default_config.py
+++ ldap_dingens/default_config.py
@@ -16,16 +16,4 @@ class DefaultConfiguration:
 
     LOGIN_LIFETIME = timedelta(days=2)
 
-    #: Host name of the LDAP server
     LDAP_SERVER = "localhost"
-
-    #: str.format string to create a DN which refers to a user with a given
-    #: loginname
-    LDAP_USER_DN_FORMAT = "uid={loginname},ou=Account,dc=fsfw-dresden,dc=de"
-
-    #: the DN to bind to for admin activity (create new users, change user
-    #: info)
-    LDAP_ADMIN_DN = "cn=AuthManager,ou=Management,dc=fsfw-dresden,dc=de"
-
-    #: set this to the password for the LDAP_ADMIN_DN above
-    LDAP_ADMIN_PASSWORD = ...
